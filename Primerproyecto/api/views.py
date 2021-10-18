a49c6f31a81fcff4eeca9ab082a4b6a5b5d57dd6
jaredolmosmb <66441195+jaredolmosmb@users.noreply.github.com>
2021-10-18 12:39:54 -0600

    limpieza de codigo

--- Primerproyecto/api/views.py
+++ Primerproyecto/api/views.py
@@ -412,9 +412,6 @@ def ProcesarView(request):
 		 responseMA = request.data
 		 responseMA1 = copy.deepcopy(responseMA)
 		 for val in responseMA['entry']:
-		 	#print("val")
-		 	#print(val)
-
 		 	if "Medication" == val['resource']['resourceType']:
 		 		if 'code' in val['resource']:
 		 			#data = normalize(val['resource']['code'].encode("latin-1").decode("utf-8"))
@@ -426,13 +423,11 @@ def ProcesarView(request):
 				 			con = ConceptS.objects.get(id = i.conceptid)
 				 			if con.active == '0':
 				 				descripciones = descripciones.exclude(id=i.id)
-				 			#print(i.term, i.conceptid, con.active)
 				 	if sinonimos.count() > 1:
 			 			for i in sinonimos:
 				 			con = ConceptS.objects.get(id = i.conceptid)
 				 			if con.active == '0':
 				 				sinonimos = sinonimos.exclude(id=i.id)
-				 			#print(i.term, i.conceptid, con.active)
 			 		if descripciones:
 			 			concepto = ConceptS.objects.get(id = descripciones[0].conceptid)
 			 			if concepto.active == '1':
@@ -440,15 +435,11 @@ def ProcesarView(request):
 			 					"url" : "codeSNOMEDActivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"codeSNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_codeSNOMED": "activo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "codeSNOMEDInactivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"codeSNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_codeSNOMED": "inactivo"})
 			 		elif sinonimos:
 			 			concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
 			 			if concepto.active == '1':
@@ -456,21 +447,16 @@ def ProcesarView(request):
 			 					"url" : "codeSNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"codeSNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"status_codeSNOMED": "activo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "codeSNOMEDInactivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"codeSNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"status_codeSNOMED": "inactivo"})
 			 		else:
 			 			val['resource'].update( {"extension": [{
 			 					"url" : "codeSNOMED",
 			 					"text" : 0
 			 					}]} )
-			 			#val['resource'].update({"codeSNOMED": 0})
 			 			existe = ConceptosNoEncontrados.objects.get(concepto = data)
 			 			if not existe:
 			 				ConceptosNoEncontrados.objects.create(concepto = data)
@@ -479,7 +465,6 @@ def ProcesarView(request):
 		 	if "MedicationAdministration" == val['resource']['resourceType']:
 		 		if 'dosage' in val['resource']:
 			 		if 'method' in val['resource']['dosage']:
-			 			#metodo = normalize(val['resource']['dosage']['method'].encode("latin-1").decode("utf-8"))
 				 		metodo = normalize(val['resource']['dosage']['method'])
 		 				descripciones = DescriptionS.objects.filter(term = metodo) & DescriptionS.objects.filter(category_id = 8)
 			 			sinonimos = Synonyms.objects.filter(term = metodo)
@@ -488,13 +473,11 @@ def ProcesarView(request):
 				 				con = ConceptS.objects.get(id = i.conceptid)
 				 				if con.active == '0':
 				 					descripciones = descripciones.exclude(id=i.id)
-				 				#print(i.term, i.conceptid, con.active)
 			 			if sinonimos.count() > 1:
 			 	 			for i in sinonimos:
 			 		 			con = ConceptS.objects.get(id = i.conceptid)
 			 		 			if con.active == '0':
 			 		 				sinonimos = sinonimos.exclude(id=i.id)
-			 		 			#print(i.term, i.conceptid, con.active)
 			 			if descripciones:
 			 				concepto = ConceptS.objects.get(id = descripciones[0].conceptid)
 			 				if concepto.active == '1':
@@ -502,15 +485,11 @@ def ProcesarView(request):
 			 					"url" : "methodSNOMEDActivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 					#val['resource']['dosage'].update({"methodSNOMED": descripciones[0].conceptid})
-			 					#val['resource']['dosage'].update({"status_methodSNOMED": "activo"})
 			 				else:
 			 					val['resource'].update( {"extension": [{
 			 					"url" : "methodSNOMEDInactivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 					#val['resource']['dosage'].update({"methodSNOMED": descripciones[0].conceptid})
-			 					#val['resource']['dosage'].update({"status_methodSNOMED": "inactivo"})
 			 			elif sinonimos:
 			 				concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
 			 				if concepto.active == '1':
@@ -518,21 +497,16 @@ def ProcesarView(request):
 			 					"url" : "methodSNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 					#val['resource']['dosage'].update({"methodSNOMED": sinonimos[0].conceptid})
-			 					#val['resource']['dosage'].update({"status_methodSNOMED": "activo"})
 			 				else:
 			 					val['resource'].update( {"extension": [{
 			 					"url" : "methodSNOMEDInactivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 					#val['resource']['dosage'].update({"methodSNOMED": sinonimos[0].conceptid})
-			 					#val['resource']['dosage'].update({"status_methodSNOMED": "inactivo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "methodSNOMEDInactivo",
 			 					"text" : 0
 			 					}]} ) 
-			 				#val['resource']['dosage'].update({"metodoSNOMED": 0})
 			 				existe = ConceptosNoEncontrados.objects.get(concepto = metodo)
 				 			if not existe:
 				 				print("entre en if not existe de administracion con metodo = "+ metodo)
@@ -549,13 +523,11 @@ def ProcesarView(request):
 				 				con = ConceptS.objects.get(id = i.conceptid)
 				 				if con.active == '0':
 				 					descripciones = descripciones.exclude(id=i.id)
-				 				#print(i.term, i.conceptid, con.active)
 			 			if sinonimos.count() > 1:
 			 	 			for i in sinonimos:
 			 		 			con = ConceptS.objects.get(id = i.conceptid)
 			 		 			if con.active == '0':
 			 		 				sinonimos = sinonimos.exclude(id=i.id)
-			 		 			#print(i.term, i.conceptid, con.active)
 			 			if descripciones:
 			 				concepto = ConceptS.objects.get(id = descripciones[0].conceptid)
 			 				if concepto.active == '1':
@@ -564,15 +536,11 @@ def ProcesarView(request):
 			 					"text" : descripciones[0].conceptid
 			 					} ) 
 			 					print(val['resource']['extension'])
-			 					#val['resource']['dosage'].update({"rutaSNOMED": descripciones[0].conceptid})
-			 					#val['resource']['dosage'].update({"status_rutaSNOMED": "activo"})
 			 				else:
 			 					val['resource']['extension'].append({
 			 					"url" : "rutaSNOMEDInactivo",
 			 					"text" : descripciones[0].conceptid
 			 					} ) 
-			 					#val['resource']['dosage'].update({"rutaSNOMED": descripciones[0].conceptid})
-			 					#val['resource']['dosage'].update({"status_rutaSNOMED": "inactivo"})
 		 
 		 				elif sinonimos:
 		 					concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
@@ -581,29 +549,22 @@ def ProcesarView(request):
 			 					"url" : "rutaSNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					} ) 
-		 						#val['resource']['dosage'].update({"rutaSNOMED": sinonimos[0].conceptid})
-		 						#val['resource']['dosage'].update({"status_rutaSNOMED": "activo"})
 		 					else:
 		 						val['resource']['extension'].append({
 			 					"url" : "rutaSNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					} ) 
-		 						#val['resource']['dosage'].update({"rutaSNOMED": sinonimos[0].conceptid})
-		 						#val['resource']['dosage'].update({"status_rutaSNOMED": "inactivo"})
 		 				else:
 		 					val['resource']['extension'].append({
 			 					"url" : "rutaSNOMED",
 			 					"text" : 0
 			 					} ) 
-		 					#val['resource']['dosage'].update({"rutaSNOMED": 0})
 		 					existe = ConceptosNoEncontrados.objects.get(concepto = ruta)
 				 			if not existe:
-				 				print("entre en if not existe de administracion con ruta = "+ ruta)
 				 				ConceptosNoEncontrados.objects.create(concepto = ruta)
 			 	print("--- %s seconds Resource MedicationAdministration ---" % (time.time() - start_time))
 		 	if "DiagnosticReport" == val['resource']['resourceType']:
 		 		if 'conclusionCode' in val['resource']:
-			 		#conclusion = normalize(val['resource']['conclusion'].encode("latin-1").decode("utf-8"))
 			 		conclusionCode = normalize(val['resource']['conclusionCode'])
 			 		descripciones = DescriptionS.objects.filter(term = conclusionCode) & DescriptionS.objects.filter(category_id = 6)
 			 		sinonimos = Synonyms.objects.filter(term = conclusionCode)
@@ -612,13 +573,11 @@ def ProcesarView(request):
 				 			con = ConceptS.objects.get(id = i.conceptid)
 				 			if con.active == '0':
 				 				descripciones = descripciones.exclude(id=i.id)
-				 			#print(i.term, i.conceptid, con.active)
 				 	if sinonimos.count() > 1:
 			 			for i in sinonimos:
 				 			con = ConceptS.objects.get(id = i.conceptid)
 				 			if con.active == '0':
 				 				sinonimos = sinonimos.exclude(id=i.id)
-				 			#print(i.term, i.conceptid, con.active)
 			 		if descripciones:
 			 			concepto = ConceptS.objects.get(id = descripciones[0].conceptid)
 			 			if concepto.active == '1':
@@ -626,15 +585,11 @@ def ProcesarView(request):
 			 					"url" : "conclusionCodeSNOMEDActivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"conclusionSNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_conclusionSNOMED": "activo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "conclusionCodeSNOMEDInactivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"conclusionSNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_conclusionSNOMED": "inactivo"})
 			 		elif sinonimos:
 			 			concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
 			 			if concepto.active == '1':
@@ -642,21 +597,16 @@ def ProcesarView(request):
 			 					"url" : "conclusionCodeSNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"conclusionSNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"status_conclusionSNOMED": "activo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "conclusionCodeSNOMEDInactivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"conclusionSNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"status_conclusionSNOMED": "inactivo"})
 			 		else:
 			 			val['resource'].update( {"extension": [{
 			 					"url" : "conclusionCodeSNOMED",
 			 					"text" : 0
 			 					}]} ) 
-			 			#val['resource'].update({"conclusionSNOMED": 0})
 			 			existe = ConceptosNoEncontrados.objects.get(concepto = conclusion)
 			 			if not existe:
 			 				ConceptosNoEncontrados.objects.create(concepto = conclusion)
@@ -665,7 +615,6 @@ def ProcesarView(request):
 			 		stop_words = set(stopwords.words("spanish"))
 			 		frasePrueba = frasePrueba.replace(',', '.')
 			 		tokens_frases = sent_tokenize(frasePrueba)
-			 		#print("token_frases antes de funcion", tokens_frases)
 			 		print("len(tokens_frases)", len(tokens_frases))
 			 		fraseFinal = ""
 			 		#----Procesamiento sin preprocesamiento de frases Frecuentes
@@ -726,15 +675,11 @@ def ProcesarView(request):
 		 					"url" : "codeSNOMEDActivo",
 		 					"text" : descripciones[0].conceptid
 		 					}]} ) 
-		 					#val1.update({"textSNOMED": descripciones[0].conceptid})
-		 					#val1.update({"status_textSNOMED": "activo"})
 		 				else:
 		 					val['resource'].update( {"extension": [{
 		 					"url" : "codeSNOMEDInactivo",
 		 					"text" : descripciones[0].conceptid
 		 					}]} )
-		 					#val1.update({"textSNOMED": descripciones[0].conceptid})
-		 					#val1.update({"status_textSNOMED": "inactivo"})
 		 			elif sinonimos:
 		 				concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
 		 				if concepto.active == "1":
@@ -742,28 +687,20 @@ def ProcesarView(request):
 		 					"url" : "codeSNOMEDActivo",
 		 					"text" : sinonimos[0].conceptid
 		 					}]} )
-		 					#val1.update({"textSNOMED": sinonimos[0].conceptid})
-		 					#val1.update({"status_textSNOMED": "activo"})
  						else:
  							val['resource'].update( {"extension": [{
 		 					"url" : "codeSNOMEDInactivo",
 		 					"text" : sinonimos[0].conceptid
 		 					}]} )
- 							#val1.update({"textSNOMED": sinonimos[0].conceptid})
- 							#val1.update({"status_textSNOMED": "inactivo"})
  					else:
- 						#print("entre en else de procedure con procedimeinto = "+ procedimiento)
  						val['resource'] .update( {"extension": [{
 		 					"url" : "codeSNOMED",
 		 					"text" : 0
 		 					}]} )
- 						#val1.update({"textSNOMED": 0})
  						existe = ConceptosNoEncontrados.objects.filter(concepto = procedimiento).first()
 			 			if not existe:
-			 				#print("entre en if not existe de procedure con procedimeinto = "+ procedimiento)
 			 				ConceptosNoEncontrados.objects.create(concepto = procedimiento)
 		 		for val1 in val['resource']['note']:
-		 			#procedimiento = normalize(val1['text'].encode("latin-1").decode("utf-8"))
 		 			procedimiento = normalize(val1['text'])
 		 		print("--- %s seconds Resource Procedure ---" % (time.time() - start_time))
 			 			
@@ -778,13 +715,11 @@ def ProcesarView(request):
 				 			con = ConceptS.objects.get(id = i.conceptid)
 				 			if con.active == '0':
 				 				descripciones = descripciones.exclude(id=i.id)
-				 			#print(i.term, i.conceptid, con.active)
 				 	if sinonimos.count() > 1:
 			 			for i in sinonimos:
 				 			con = ConceptS.objects.get(id = i.conceptid)
 				 			if con.active == '0':
 				 				sinonimos = sinonimos.exclude(id=i.id)
-				 			#print(i.term, i.conceptid, con.active)
 			 		if descripciones:
 			 			concepto = ConceptS.objects.get(id = descripciones[0].conceptid)
 			 			if concepto.active == '1':
@@ -792,15 +727,11 @@ def ProcesarView(request):
 			 					"url" : "categorySNOMEDActivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"categorySNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_categorySNOMED": "activo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "categorySNOMEDInactivo",
 			 					"text" : descripciones[0].conceptid
 			 					}]} )
-			 				#val['resource'].update({"categorySNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_categorySNOMED": "inactivo"})
 			 		elif sinonimos:
 			 			concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
 			 			if concepto.active == '1':
@@ -808,31 +739,24 @@ def ProcesarView(request):
 			 					"url" : "categorySNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"categorySNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"status_categorySNOMED": "activo"})
 			 			else:
 			 				val['resource'].update( {"extension": [{
 			 					"url" : "categorySNOMEDInactivo",
 			 					"text" : sinonimos[0].conceptid
 			 					}]} ) 
-			 				#val['resource'].update({"categorySNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"status_categorySNOMED": "inactivo"})
 
 			 		else:
 			 			val['resource'].update( {"extension": [{
 			 					"url" : "categorySNOMED",
 			 					"text" : 0
 			 					}]} )
-			 			#val['resource'].update({"categorySNOMED": 0})
 			 			existe = ConceptosNoEncontrados.objects.filter(concepto = categoria).first()
 			 			if not existe:
 			 				ConceptosNoEncontrados.objects.create(concepto = categoria)
 
 		 		if 'code' in val['resource']:
-			 		#code = normalize(val['resource']['code'].encode("latin-1").decode("utf-8"))
 			 		code = normalize(val['resource']['code'])
 			 		descripciones = DescriptionS.objects.filter(term = code)
-			 		#print("descripciones: termino--esta activo?" + code)
 			 		sinonimos = Synonyms.objects.filter(term = code)
 			 		if descripciones.count() > 1:
 			 			for i in descripciones:
@@ -853,15 +777,11 @@ def ProcesarView(request):
 			 					"url" : "codeSNOMEDActivo",
 			 					"text" : descripciones[0].conceptid
 			 					} ) 
-			 				#val['resource'].update({"codeSNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_codeSNOMED": "activo"})
 			 			else:
 			 				val['resource']['extension'].append({
 			 					"url" : "codeSNOMEDInactivo",
 			 					"text" : descripciones[0].conceptid
 			 					} ) 
-			 				#val['resource'].update({"codeSNOMED": descripciones[0].conceptid})
-			 				#val['resource'].update({"status_codeSNOMED": "inactivo"})
 			 		elif sinonimos:
 			 			concepto = ConceptS.objects.get(id = sinonimos[0].conceptid)
 			 			if concepto.active == '1':
@@ -869,21 +789,16 @@ def ProcesarView(request):
 			 					"url" : "codeSNOMEDActivo",
 			 					"text" : sinonimos[0].conceptid
 			 					} ) 
-			 				#val['resource'].update({"codeSNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"codeSNOMED": "activo"})
 			 			else:
 			 				val['resource']['extension'].append({
 			 					"url" : "codeSNOMEDInactivo",
 			 					"text" : sinonimos[0].conceptid
 			 					} ) 
-			 				#val['resource'].update({"codeSNOMED": sinonimos[0].conceptid})
-			 				#val['resource'].update({"codeSNOMED": "inactivo"})
 	 				else:
 	 					val['resource']['extension'].append({
 			 					"url" : "codeSNOMED",
 			 					"text" : 0
 			 					} ) 
-	 					#val['resource'].update({"codeSNOMED": 0})
 	 					existe = ConceptosNoEncontrados.objects.get(concepto = code)
 			 			if not existe:
 			 				ConceptosNoEncontrados.objects.create(concepto = code)
